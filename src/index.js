1fd337f61b5f8bb45de17154cb21cb7dde37c301
DartRock <64318620+DartRock@users.noreply.github.com>
2021-12-09 09:30:48 +0200

    completed markup, styles and scripts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import './js/main';
 import './sass/main.scss';
+
+import './js/scroll';
+import './js/up-btn';
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.new-product__card-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
